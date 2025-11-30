f5c615cb4fcbf701320803b5dfae8d7b3d413b5b
Robert98A <robert.amartisoaei@hotmail.com>
2025-12-01 01:57:57 +0200

    Update 02_merge_tables.sql

--- Case_Studies/Cyclistic/Scripts/SQL/02_merge_tables.sql
+++ Case_Studies/Cyclistic/Scripts/SQL/02_merge_tables.sql
@@ -1,1 +1,10 @@
-
+CREATE OR REPLACE TABLE cyclistic-analysis-479022.cyclistic_case_study.merged_tripdata AS
+SELECT * FROM cyclistic-analysis-479022.cyclistic_case_study.clean_2019_Q1
+UNION ALL
+SELECT * FROM cyclistic-analysis-479022.cyclistic_case_study.clean_2019_Q2
+UNION ALL
+SELECT * FROM cyclistic-analysis-479022.cyclistic_case_study.clean_2019_Q3
+UNION ALL
+SELECT * FROM cyclistic-analysis-479022.cyclistic_case_study.clean_2019_Q4
+UNION ALL
+SELECT * FROM cyclistic-analysis-479022.cyclistic_case_study.clean_2020_Q1;
