e854f69de66b2eaca6d3e4beeddf9f3a3b3e9c4c
Robert98A <robert.amartisoaei@hotmail.com>
2025-12-01 02:03:30 +0200

    Update monthly_rides.sql

--- Case_Studies/Cyclistic/Scripts/SQL/03_statistical_analysis/monthly_rides.sql
+++ Case_Studies/Cyclistic/Scripts/SQL/03_statistical_analysis/monthly_rides.sql
@@ -1,1 +1,9 @@
-
+-- Checking monthly ride trends
+SELECT
+  member_casual,
+  EXTRACT(YEAR FROM started_at) AS year,
+  EXTRACT(MONTH FROM started_at) AS month,
+  COUNT(*) AS rides
+FROM cyclistic-analysis-479022.cyclistic_case_study.merged_tripdata
+GROUP BY member_casual, year, month
+ORDER BY year, month, member_casual;
