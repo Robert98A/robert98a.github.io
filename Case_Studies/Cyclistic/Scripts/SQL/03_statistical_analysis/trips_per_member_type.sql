c8664bf70a907a98cd54712344f6ae858faeae06
Robert98A <robert.amartisoaei@hotmail.com>
2025-12-01 02:02:08 +0200

    Update trips_per_member_type.sql

--- Case_Studies/Cyclistic/Scripts/SQL/03_statistical_analysis/trips_per_member_type.sql
+++ Case_Studies/Cyclistic/Scripts/SQL/03_statistical_analysis/trips_per_member_type.sql
@@ -1,1 +1,6 @@
-
+-- Checking number of rides by rider type
+SELECT
+  member_casual,
+  COUNT(*) AS total_rides
+FROM cyclistic-analysis-479022.cyclistic_case_study.merged_tripdata
+GROUP BY member_casual;
