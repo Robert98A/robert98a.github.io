20b86353f52f2b9efa43f4b645f794cf9f4a2a74
Robert98A <robert.amartisoaei@hotmail.com>
2025-12-01 02:02:56 +0200

    Update time_of_day.sql

--- Case_Studies/Cyclistic/Scripts/SQL/03_statistical_analysis/time_of_day.sql
+++ Case_Studies/Cyclistic/Scripts/SQL/03_statistical_analysis/time_of_day.sql
@@ -1,1 +1,8 @@
-
+-- Checking time of day patterns
+SELECT
+  member_casual,
+  EXTRACT(HOUR FROM started_at) AS hour,
+  COUNT(*) AS rides
+FROM cyclistic-analysis-479022.cyclistic_case_study.merged_tripdata
+GROUP BY member_casual, hour
+ORDER BY member_casual, hour;
