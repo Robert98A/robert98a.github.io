b5fc3cf82f6ec7ae7f6895555f6ca546d70545ab
Robert98A <robert.amartisoaei@hotmail.com>
2025-12-01 02:02:26 +0200

    Update avg_ride_length.sql

--- Case_Studies/Cyclistic/Scripts/SQL/03_statistical_analysis/avg_ride_length.sql
+++ Case_Studies/Cyclistic/Scripts/SQL/03_statistical_analysis/avg_ride_length.sql
@@ -1,1 +1,6 @@
-
+-- Checking average ride length
+SELECT
+  member_casual,
+  AVG(ride_length_minutes) AS avg_ride_length
+FROM cyclistic-analysis-479022.cyclistic_case_study.merged_tripdata
+GROUP BY member_casual;
