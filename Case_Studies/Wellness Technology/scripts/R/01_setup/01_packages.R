d3fb1a196e530cb1a70f34beb0cf6aa14128c37c
Robert Amartisoaei <robert.amartisoaei@hotmail.com>
2025-12-17 02:19:37 +0200

    Update 01_packages.R

--- Case_Studies/Wellness Technology/scripts/R/01_setup/01_packages.R
+++ Case_Studies/Wellness Technology/scripts/R/01_setup/01_packages.R
@@ -1,1 +1,26 @@
+############################################################
+# 01_packages.R
+# Purpose: Install and load required packages
+############################################################
 
+packages <- c(
+  "tidyverse",
+  "janitor",
+  "skimr",
+  "naniar",
+  "lubridate"
+)
+
+installed <- rownames(installed.packages())
+
+for (p in packages) {
+  if (!p %in% installed) {
+    install.packages(p)
+  }
+}
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(skimr)
+library(naniar)
